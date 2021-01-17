613c2918adcfc48bcd657511839a815bd5138f59
hong li <lihongitchina@gmail.com>
2021-01-17 17:27:38 +0800

    if-else switch fstream

---
+++ c++/src/6.8cctype.cpp
@@ -0,0 +1,36 @@
+// cctypes.cpp -- using the ctype.ch library 
+#include <iostream>
+#include <cctype> // prototypes for character functions 
+int main(){
+    using namespace std; 
+    cout << "Enter text for analysis, and type @"
+        "to terminate input.\n";
+    char ch;
+    int whitespace = 0; 
+    int digits = 0; 
+    int chars = 0; 
+    int punct = 0; 
+    int others = 0;
+
+    cin.get(ch); // get first character 
+    while(ch != '@')
+    {
+        if (isalpha(ch)) // is it an alphabetic character?
+            chars++;
+        else if (isspace(ch)) // is it a whitespace character?
+            whitespace++;
+        else if (isdigit(ch)) // is it a digit ?
+            digits++;
+        else if (ispunct(ch)) // is it punctuation?
+            punct++;
+        else 
+            others++;  
+        cin.get(ch); // get next character
+    }
+    cout << chars << " letters."
+        << whitespace << " whitespace."
+        << digits << " digits."
+        << punct << " punctuations."
+        << others << " others.\n";
+return 0;
+}
